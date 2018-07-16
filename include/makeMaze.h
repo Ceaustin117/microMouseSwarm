0a4c23948e645b3dfdef81f554195722280282e1
Ceaustin117 <ceaustin117@gmail.com>
2018-07-16 16:51:43 -0400

    Create makeMaze.h

---
+++ include/makeMaze.h
@@ -0,0 +1,4 @@
+void copyGrid(int boundsX, int boundsY,int **grid1, int **grid2 );
+void resetTargetGrid(int boundsX, int boundsY,int **targetGrid);
+void carveMaze(int **maze, int width, int height, int x, int y);
+void generateMaze(int **maze, int width, int height);
