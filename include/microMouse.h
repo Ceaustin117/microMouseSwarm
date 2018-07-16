8c9a4f2a34a75b67ae4de76fad82fe39ab040966
Ceaustin117 <ceaustin117@gmail.com>
2018-07-16 16:52:43 -0400

    Create microMouse.h

---
+++ include/microMouse.h
@@ -0,0 +1,3 @@
+int checkNearOthermicroMouse(microMouseBot * microMouse, microMouseBot * microMouse2);
+void microMouseShareMapping(int boundsX, int boundsY, microMouseBot * microMouse, microMouseBot * microMouse2);
+void resetmicroMouseBot(microMouseBot * microMouse, int boundsX, int boundsY);
