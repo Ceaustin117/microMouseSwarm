6b43b6462c873bb322e4c11371c411aaeae76750
Ceaustin117 <ceaustin117@gmail.com>
2018-07-16 16:56:06 -0400

    Create printers.c

---
+++ include/printers.c
@@ -0,0 +1,6 @@
+void printGrid(int boundsX, int boundsY,int **grid);
+void printmicroMouseGrid(int boundsX, int boundsY, int * * grid, microMouseBot * microMouse);
+void printMicroMouseArrayGrid(int boundsX, int boundsY, int * * grid, microMouseBot ** microMouseArray);
+int checkMicroMouseArray(int i,int j, microMouseBot ** microMouseArray);
+int checkMicroMouseArrayGrids(int i,int j, microMouseBot ** microMouseArray);
+int checkFirstMicroMouseBot(int i,int j,microMouseBot ** microMouseArray);
