d9a79395f91a0fa64942094a9fb4971fb0905b2d
Ceaustin117 <ceaustin117@gmail.com>
2018-07-17 11:44:13 -0400

    Delete microMouse.h

--- include/microMouse.h
+++
@@ -1,3 +0,0 @@
-int checkNearOthermicroMouse(microMouseBot * microMouse, microMouseBot * microMouse2);
-void microMouseShareMapping(int boundsX, int boundsY, microMouseBot * microMouse, microMouseBot * microMouse2);
-void resetmicroMouseBot(microMouseBot * microMouse, int boundsX, int boundsY);
