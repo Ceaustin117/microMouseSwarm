522519375b37cb98012c9fb39366caca69df66d5
Ceaustin117 <ceaustin117@gmail.com>
2018-07-17 11:44:21 -0400

    Delete makeMaze.h

--- include/makeMaze.h
+++
@@ -1,4 +0,0 @@
-void copyGrid(int boundsX, int boundsY,int **grid1, int **grid2 );
-void resetTargetGrid(int boundsX, int boundsY,int **targetGrid);
-void carveMaze(int **maze, int width, int height, int x, int y);
-void generateMaze(int **maze, int width, int height);
