8330bc2a0e63756216edaf1bad62902716acaa1f
Ceaustin117 <ceaustin117@gmail.com>
2018-07-17 11:43:43 -0400

    Delete wallFollow.h

--- include/wallFollow.h
+++
@@ -1,5 +0,0 @@
-void wallFollowRightStrong(int **maze, int width, int height,microMouseBot *microMouse, int microMouseNum);
-void wallFollowLeftStrong(int **maze, int width, int height,microMouseBot *microMouse, int microMouseNum);
-void wallFollowLeft(int **maze, int width, int height,microMouseBot *microMouse);
-void wallFollowLeft2(int **maze, int width, int height,microMouseBot *microMouse);
-void wallFollowRight(int **maze, int width, int height,microMouseBot *microMouse);
