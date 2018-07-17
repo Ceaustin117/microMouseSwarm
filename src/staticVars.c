97f914bd4fbb9f4b68e8b9dcad7730d04ba2c82c
Ceaustin117 <ceaustin117@gmail.com>
2018-07-17 11:39:56 -0400

    Create staticVars.c

---
+++ src/staticVars.c
@@ -0,0 +1,63 @@
+#include "staticVars.h"
+//static variables
+static int boundsX;
+static int boundsY;
+static int numMoves;
+static int numTests;
+static int numBots;
+static int algChoice;
+//Setters
+void setAlgChoice (int a)
+{
+	algChoice = a;
+};
+void setBoundsX (int x)
+{
+	boundsX = x;
+};
+void setBoundsY (int y)
+{
+	boundsY = y;
+};
+void setNumMoves (int m)
+{
+	numMoves = m;
+};
+int setNumTests(int n)
+{
+	numTests = n;
+};
+int setNumBots(int n)
+{
+	numBots = n;
+};
+//Getters
+int getBoundsX (void)
+{
+	return boundsX;
+};
+int getBoundsY (void)
+{
+	return boundsY;
+};
+int getNumMoves (void)
+{
+	return numMoves;
+}
+int getNumTests(void)
+{
+	return numTests;
+};
+int getNumBots(void)
+{
+	return numBots;
+};
+int getAlgChoice(void)
+{
+	return algChoice;
+}
+//Adders
+void addToNumMoves (int s)
+{
+	numMoves = numMoves + s;
+};
