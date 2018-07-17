21bbaeb2f4b4fb803996a62f4625474b8e763974
Ceaustin117 <ceaustin117@gmail.com>
2018-07-17 11:44:05 -0400

    Delete movement.h

--- include/movement.h
+++
@@ -1,7 +0,0 @@
-int checkForWallOrBot(int * * maze, int nx, int ny);
-int mazeFullyMapped(int * * maze, int boundsX, int boundsY);
-void placemicroMouseOnRandomSpot(int * * maze, int boundsX, int boundsY, microMouseBot * microMouse);
-int countOpen(int * * maze, int width, int height);
-void microMouseArrayMapSharing(int** targetGrid,int  boundsX,int  boundsY,microMouseBot** microMouseArray,int index);
-void moveMicroMouseArray(int **targetGrid, int boundsX, int boundsY, microMouseBot** microMouseArray);
-int countStepsFunc();
