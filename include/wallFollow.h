bbd2da2f1fe80f5d7d7d13942f86dd0e41da863d
Ceaustin117 <ceaustin117@gmail.com>
2018-07-18 19:31:49 -0400

    Update wallFollow.h

--- include/wallFollow.h
+++ include/wallFollow.h
@@ -3,10 +3,22 @@
 
 #include "microMouse.h"
 
-extern void wallFollowRightStrong(int **maze, int microMouseNum, microMouseBot *microMouse);
-extern void wallFollowLeftStrong(int **maze, int microMouseNum, microMouseBot *microMouse);
-extern void wallFollowLeft(int **maze, microMouseBot *microMouse);
-extern void wallFollowLeft2(int **maze, microMouseBot *microMouse);
-extern void wallFollowRight(int **maze, microMouseBot *microMouse);
+#include "staticVars.h"
+#include "movement.h"
+#include "maze.h"
+
+extern void wallFollowRight(int **maze,  int microMouseNum, microMouseBot *microMouse);
+extern void wallFollowLeft(int **maze,int microMouseNum, microMouseBot *microMouse);
+
+static void wallFollowMove(int **maze, int microMouseNum, microMouseBot *microMouse);
+static void wallFollowMoveInDir(int **maze, microMouseBot *microMouse);
+static void wallFollowRightFacingRightMove(int **maze, int microMouseNum, microMouseBot *microMouse);
+static void wallFollowRightFacingLeftMove(int **maze, int microMouseNum, microMouseBot *microMouse);
+static void wallFollowRightFacingUpMove(int **maze, int microMouseNum, microMouseBot *microMouse);
+static void wallFollowRightFacingDownMove(int **maze, int microMouseNum, microMouseBot *microMouse);
+static void wallFollowLeftFacingRightMove(int **maze, int microMouseNum, microMouseBot *microMouse);
+static void wallFollowLeftFacingLeftMove(int **maze, int microMouseNum, microMouseBot *microMouse);
+static void wallFollowLeftFacingUpMove(int **maze, int microMouseNum, microMouseBot *microMouse);
+static void wallFollowLeftFacingDownMove(int **maze, int microMouseNum, microMouseBot *microMouse);
 
 #endif /* WALLFOLLOW_H */
