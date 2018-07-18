831601b3d46592e09a459331ae9ae7871b7cf877
Ceaustin117 <ceaustin117@gmail.com>
2018-07-18 19:33:32 -0400

    Update microMouse.h

--- include/microMouse.h
+++ include/microMouse.h
@@ -16,15 +16,16 @@ typedef struct microMouseBot
 	int **microMouseMappedMaze;
 }microMouseBot;
 
-extern void microMouseShareMapping(microMouseBot *microMouse, microMouseBot *microMouse2);
+extern void microMouseMakeArray(int **maze,microMouseBot **microMouseArray);
 extern void microMouseBotReset(microMouseBot *microMouse);
-extern void microMouseShareMapping(microMouseBot *microMouse, microMouseBot *microMouse2);
-extern void microMouseArrayMapSharing(int** targetGrid, int index, microMouseBot **microMouseArray);
-extern void microMousePlaceOnRandomSpot(int **maze, microMouseBot *microMouse);
 extern int microMouseArrayCheck(int i,int j, microMouseBot **microMouseArray);
 extern int microMouseArrayMazeCheck(int i,int j, microMouseBot **microMouseArray);
 extern int microMouseArrayCheckFirst(int i, int j, microMouseBot ** microMouseArray);
-extern int microMouseCheckForNeighbor(microMouseBot *microMouse, microMouseBot *microMouse2);
+extern void microMouseArrayMapSharing(int** targetGrid, int index, microMouseBot **microMouseArray);
+extern void microMousePlaceOnRandomSpot(int **maze, microMouseBot *microMouse);
+
+static int microMouseCheckForNeighbor(microMouseBot *microMouse, microMouseBot *microMouse2);
+static void microMouseShareMapping(microMouseBot *microMouse, microMouseBot *microMouse2);
 
 #endif /* MICROMOUSE_H */
 
