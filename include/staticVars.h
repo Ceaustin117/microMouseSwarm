3868db3dd66115f3a380fd713332ada4d285037b
Ceaustin117 <ceaustin117@gmail.com>
2018-07-18 19:32:27 -0400

    Update staticVars.h

--- include/staticVars.h
+++ include/staticVars.h
@@ -1,19 +1,22 @@
 #ifndef STATICVARS_H
 #define STATICVARS_H
 
-//setters
+//Setters
+extern void setAlgChoice (int a);
 extern void setBoundsX (int x);
 extern void setBoundsY (int y);
 extern void setNumMoves (int m);
+extern void setNumTests(int n);
 extern void setNumBots(int n);
-extern void setAlgChoice(int a);
-//adders
-extern void addToNumMoves (int m);
-//getters
-extern int getBoundsX(void);
-extern int getBoundsY(void);
-extern int getNumMoves(void);
+//Getters
+extern int getBoundsX (void);
+extern int getBoundsY (void);
+extern int getNumMoves (void);
+extern int getNumTests(void);
 extern int getNumBots(void);
 extern int getAlgChoice(void);
+//Adders
+extern void addToNumMoves (int s);
 
 #endif /* STATICVARS_H */
+
