2cce7eb4356e058bf307b85127e7dce8a3e5dee6
Ceaustin117 <ceaustin117@gmail.com>
2018-07-18 19:33:49 -0400

    Update maze.h

--- include/maze.h
+++ include/maze.h
@@ -5,7 +5,8 @@ extern int mazeFullyMapped(int **maze);
 extern int mazeCheckForWallOrBot(int **maze, int nx, int ny);
 extern void mazeCopy(int **maze1, int **maze2 );
 extern void mazeReset(int **maze);
-extern void mazeCarve(int **maze, int x, int y);
 extern void mazeGenerate(int **maze);
+
+static void mazeCarve(int **maze, int x, int y);
 
 #endif /* MAZE_H */
